5ff9c543f850730aed4d415ddcbc2708fbbf52da
jeremy boulin <jeremy.boulin@gmail.com>
2024-07-15 18:03:40 +0200

    controllers remplis

--- app/controllers/plant_lists_controller.rb
+++ app/controllers/plant_lists_controller.rb
@@ -1,2 +1,43 @@
 class PlantListsController < ApplicationController
+  before_action :set_plant_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @plant_lists = PlantList.all
+  end
+
+  def show
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @plant_list = @list.plant_lists.build(plant_list_params)
+    if @plant_list.save
+      redirect_to @list, notice: 'Plant was successfully added to the list.'
+    else
+      render 'lists/show'
+    end
+  end
+
+  def update
+    if @plant_list.update(plant_list_params)
+      redirect_to @plant_list, notice: 'PlantList was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @plant_list.destroy
+    redirect_to plant_lists_url, notice: 'PlantList was successfully removed.'
+  end
+
+  private
+
+  def set_plant_list
+    @plant_list = PlantList.find(params[:id])
+  end
+
+  def plant_list_params
+    params.require(:plant_list).permit(:plant_id, :list_id)
+  end
 end
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,2 +1,43 @@
 class TipsController < ApplicationController
+  before_action :set_tip, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tips = Tip.all
+  end
+
+  def show
+  end
+
+  def create
+    @plant = Plant.find(params[:plant_id])
+    @tip = @plant.tips.build(tip_params)
+    if @tip.save
+      redirect_to @plant, notice: 'Tip was successfully created.'
+    else
+      render 'plants/show'
+    end
+  end
+
+  def update
+    if @tip.update(tip_params)
+      redirect_to @tip, notice: 'Tip was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tip.destroy
+    redirect_to tips_url, notice: 'Tip was successfully destroyed.'
+  end
+
+  private
+
+  def set_tip
+    @tip = Tip.find(params[:id])
+  end
+
+  def tip_params
+    params.require(:tip).permit(:titre, :contenu, :user_id, :plant_id, :status)
+  end
 end
