ef7143d597c25d8614be7d4d6f24f1b2fdf0ab95
jeremy boulin <jeremy.boulin@gmail.com>
2024-07-23 11:35:24 +0200

    liens contact direct en mailto

--- app/javascript/controllers/edit_tip_controller.js
+++ app/javascript/controllers/edit_tip_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit-tip"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "messages"]
   connect() {
     console.log("hello");
     console.log(this.formTarget);
@@ -12,5 +12,21 @@ export default class extends Controller {
     console.log("click");
 
     this.formTarget.classList.toggle("d-none")
+  }
+
+  handleSuccess(event) {
+    const [data, status, xhr] = event.detail
+    this.insertMessageAndScrollDown(data)
+    this.formTarget.reset() // Réinitialise le formulaire après soumission
+  }
+
+  insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.scrollToBottom()
+  }
+
+  scrollToBottom() {
+    // Scrolls to the bottom of the messages container
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight
   }
 }
